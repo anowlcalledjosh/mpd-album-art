ed5e5487240db0f322503b579b70388eec45ce08
Josh Holland <anowlcalledjosh@gmail.com>
2017-07-22 15:06:49 +0100

    Fix most flake8 errors

--- mpd_album_art.py
+++ mpd_album_art.py
@@ -1,17 +1,19 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+
 # MPD Album Art
-# This program is free software: you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License as published 
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 :copyright: 2013 Jamie Macdonald
 :license: GNU General Public License (GPL) version 3
@@ -25,7 +27,9 @@
 from pylast import LastFMNetwork, AlbumSearch
 from mpd import MPDClient, socket
 
-import os, sys
+import os
+import sys
+
 try:
     # Python 3
     from urllib.request import urlretrieve
@@ -42,15 +46,15 @@ _last_fm_api_key = "6b6d8768f0c41f7e82da0de44e1db76a"
 
 class Grabber(object):
     """An object to grab artwork for MPD songs
-    
-    :param str save_dir: 
+
+    :param str save_dir:
         Directory into which Grabber should download new images.
     :param str library_dir:
         Directory MPD is currently playing from.
     :param str link_path:
         Path to symlink to current image file. By default,
         ``os.path.join(save_dir, "current")``
-    
+
     Construct a Grabber with ``save_dir = ~/.covers``:
 
     >>> import os
@@ -69,7 +73,7 @@ class Grabber(object):
         super(Grabber, self).__init__()
 
         self.save_dir = save_dir
-        
+
         if library_dir is None:
             library_dir = os.path.join(os.environ['HOME'], 'Music')
         self.library_dir = library_dir
@@ -87,9 +91,9 @@ class Grabber(object):
         Do not overwrite existing files.
         Set ``os.path.join(save_dir, 'current')`` as symlink to image file.
 
-        :param dict song: 
+        :param dict song:
             A dictionary with keys ``'album'`` and ``'artist'`` to correspond
-            with string representations of the album and artist (resp.) of 
+            with string representations of the album and artist (resp.) of
             interest. Use ``MPDClient.currentsong()`` to return uch a dictionary
             .
         :return:
@@ -111,7 +115,7 @@ class Grabber(object):
             return file_path
 
         # Define the search network compatible with LastFM API
-        network = LastFMNetwork(api_key = _last_fm_api_key)
+        network = LastFMNetwork(api_key=_last_fm_api_key)
 
         album_search = AlbumSearch(song['album'], network)
 
@@ -154,11 +158,11 @@ class Grabber(object):
 
         :param dict song:
             A dictionary with keys ``'album'`` and ``'artist'`` to correspond
-            with string representations of the album and artist (resp.) of 
+            with string representations of the album and artist (resp.) of
             interest. Such a dictionary returns from
             ``MPDClient.currentsong()``
         :return:
-            A string representation of the local file path to the largest image 
+            A string representation of the local file path to the largest image
             file for ``song`` found in ``song_folder``, or ``None`` if no results
             found
         """
@@ -180,7 +184,6 @@ class Grabber(object):
             self.set_current_link(file_path)
         return file_path
 
-
     def remove_current_link(self):
         """Delete current artwork symlink.
         """
@@ -201,15 +204,15 @@ class Grabber(object):
 
         :param dict song:
             A dictionary with keys ``'album'`` and ``'artist'`` to correspond
-            with string representations of the album and artist (resp.) of 
+            with string representations of the album and artist (resp.) of
             interest. Such a dictionary returns from
             ``MPDClient.currentsong()``
         :param str img_url:
             A string representation of an external url points to the album
             artwork image file
-        :return: 
+        :return:
             ``str`` of the form 'Artist_Name_Album_Name_h45h.png', or if
-            ``img_url`` is ``None``, simply 'Artist_Name_Album_Name'. In this 
+            ``img_url`` is ``None``, simply 'Artist_Name_Album_Name'. In this
             example, ``'h45h.png'`` extracts from the original filename in
             ``img_url``.
         """
@@ -241,7 +244,8 @@ def _sanitize(name):
 
 
 def main():
-    import argparse, os
+    import argparse
+    import os
 
     home_dir = os.environ['HOME']
     parser = argparse.ArgumentParser()
@@ -278,7 +282,7 @@ def main():
     # try local pics
     elif grabber.get_local_art(current_song) is not None:
         sys.stderr.write('Found local image, not querying LastFM.\n')
-    
+
     # try lastFM pics
     elif grabber.get_art(current_song) is not None:
         sys.stderr.write('Found lastFM image.\n')
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 setup(
-    name = "MPD-Album-Art",
-    version = "0.1",
-    author = "Jamie Macdonald",
-    author_email = "jamie.alban@gmail.com",
-    description = "Module to fetch Album Art for currently playing song on Music PLayer Daemon",
-    license = "GPL 3.0",
+    name="MPD-Album-Art",
+    version="0.1",
+    author="Jamie Macdonald",
+    author_email="jamie.alban@gmail.com",
+    description="Module to fetch Album Art for currently playing song on Music PLayer Daemon",
+    license="GPL 3.0",
 
-    url = "http://jameh.github.io/mpd-album-art",
+    url="http://jameh.github.io/mpd-album-art",
 
-    py_modules = ['mpd_album_art'],
-    entry_points = {
+    py_modules=['mpd_album_art'],
+    entry_points={
         'console_scripts': ["mpd-album-art=mpd_album_art:main"],
     },
-    install_requires = ['python-mpd2', 'pylast'],
+    install_requires=['python-mpd2', 'pylast'],
 
-    keywords = "MPD, Album Art, LastFM",
+    keywords="MPD, Album Art, LastFM",
 
     # could also include long_description, download_url, classifiers, etc.
 )
